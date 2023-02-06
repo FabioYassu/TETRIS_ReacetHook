1cfd40550d7dacc554872cff54e65c581b42c004
Fabio Yassu <fabio.yassu@gmail.com>
2023-02-06 08:36:55 -0300

    Feat: Adicionado contador de pontos

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 
 import { createStage, checkCollision } from '../gameHelpers';
-
-// Styled Components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 // Custom Hooks
+import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
+import { useGameStatus } from '../hooks/useGameStatus';
 
 // Components
 import Stage from './Stage';
@@ -19,7 +19,10 @@ const Tetris = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-  const [stage, setStage] = useStage(player, resetPlayer);
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(
+    rowsCleared
+  );
 
   console.log('re-render');
 
@@ -27,33 +30,59 @@ const Tetris = () => {
     if (!checkCollision(player, stage, { x: dir, y: 0 })) {
       updatePlayerPos({ x: dir, y: 0 });
     }
-  }
+  };
+
+  const keyUp = ({ keyCode }) => {
+    if (!gameOver) {
+      //Ative o intervalo novamente quando ao soltar a seta para baixo.
+      if (keyCode === 40) {
+        setDropTime(1000 / (level + 1));
+      }
+    }
+  };
 
   const startGame = () => {
-    console.log("test")
-    // Reset everything
+    //Reset
     setStage(createStage());
+    setDropTime(1000);
     resetPlayer();
+    setScore(0);
+    setLevel(0);
+    setRows(0);
     setGameOver(false);
-  }
+  };
 
   const drop = () => {
+    //Aumentar o nível quando o jogador tiver limpado 10 linhas
+    if (rows > (level + 1) * 10) {
+      setLevel(prev => prev + 1);
+      //Aumentar Velocidade
+      setDropTime(1000 / (level + 1) + 200);
+    }
+
     if (!checkCollision(player, stage, { x: 0, y: 1 })) {
-      updatePlayerPos({ x: 0, y: 1, collided: false })
+      updatePlayerPos({ x: 0, y: 1, collided: false });
     } else {
-      // Game Over
+      // Game over!
       if (player.pos.y < 1) {
-        console.log("GAME OVER!!!");
+        console.log('GAME OVER!!!');
         setGameOver(true);
         setDropTime(null);
       }
       updatePlayerPos({ x: 0, y: 0, collided: true });
     }
-  }
+  };
 
   const dropPlayer = () => {
+    //Não precisamos executar o intervalo quando usamos a seta para baixo.   
+    setDropTime(null);
     drop();
-  }
+  };
+
+  // Aqui começa o jogo 
+  useInterval(() => {
+    drop();
+  }, dropTime);
 
   const move = ({ keyCode }) => {
     if (!gameOver) {
@@ -63,14 +92,19 @@ const Tetris = () => {
         movePlayer(1);
       } else if (keyCode === 40) {
         dropPlayer();
-      } else if (keyCode === 38 ) {
+      } else if (keyCode === 38) {
         playerRotate(stage, 1);
       }
     }
-  }
+  };
 
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+    <StyledTetrisWrapper
+      role="button"
+      tabIndex="0"
+      onKeyDown={e => move(e)}
+      onKeyUp={keyUp}
+    >
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -78,9 +112,9 @@ const Tetris = () => {
             <Display gameOver={gameOver} text="Game Over" />
           ) : (
             <div>
-              <Display text="Score" />
-              <Display text="Rows" />
-              <Display text="Level" />
+              <Display text={`Score: ${score}`} />
+              <Display text={`rows: ${rows}`} />
+              <Display text={`Level: ${level}`} />
             </div>
           )}
           <StartButton callback={startGame} />
